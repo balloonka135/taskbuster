135ce682c6b979683bb9025c08fd02837f5dd843
balloonka135 <balloonka135@gmail.com>
2017-09-15 22:43:25 +0300

    Internationalization and localization

--- functional_tests/test_all_users.py
+++ functional_tests/test_all_users.py
@@ -2,6 +2,8 @@
 
 from selenium import webdriver
 from django.core.urlresolvers import reverse
+from django.utils import formats
+from django.utils.translation import activate
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 
@@ -17,16 +19,49 @@ class HomeNewVisitorTest(StaticLiveServerTestCase):
     def get_full_url(self, namespace):
         return self.live_server_url + reverse(namespace)
 
-    def test_gome_title(self):
+    def test_home_title(self):
         self.browser.get(self.get_full_url(self.browser.title))
         self.assertIn("taskBuster", self.browser.title)
 
     def test_h1_css(self):
         self.browser.get(self.get_full_url("home"))
         h1 = self.browser.find_element_by_tag_name("h1")
-        self.assertEqual(h1.value_of_css_property("color"), "rgba(200, 50, 255, 1)")
+        self.assertEqual(h1.value_of_css_property("color"), u"rgb(200, 50, 255)")
+
+    def test_home_files(self):
+        self.browser.get(self.live_server_url + '/robots.txt')
+        self.assertNotIn("Not Found", self.browser.title)
+        self.browser.get(self.live_server_url + '/humans.txt')
+        self.assertNotIn("Not Found", self.browser.title)
+
+    def test_localization(self):
+        today = date.today()
+        for lang in ['en', 'de']:
+            activate(lang)
+            self.browser.get(self.get_full_url("home"))
+            local_date = self.browser.find_element_by_id('non-local-date')
+            self.assertEqual(formats.date_format(today, use_l10n=True), local_date.text)
+            self.assertEqual(today.strftime('%Y-%m-%d'), non_local_date.text)
+
+    def test_time_zone(self):
+        self.browser.get(self.get_full_url("home"))
+        tz = self.browser.find_element_by_id("time-tz").text
+        utc = self.browser.find_element_by_id("time-utc").text
+        ny = self.browser.find_element_by_id("time-ny").text
+        self.assertNotEqual(tz, utc)
+        self.assertNotIn(ny, [tz, utc])
 
 
 if __name__ == '__main__':
     unittest.main()
+
+
+# run unit and functional tests using coverage
+# coverage run --source='.' manage.py test
+# coverage report
+
+
+
+
+
 
--- taskbuster/test.py
+++ taskbuster/test.py
@@ -2,11 +2,13 @@
 
 from django.test import TestCase
 from django.core.urlresolvers import reverse
+from django.utils.translation import activate
 
 
 class TestHomePage(TestCase):
 
     def test_uses_index_template(self):
+        activate('en')
         response = self.client.get(reverse('home'))
         self.assertTemplateUsed(response, 'taskbuster/index.html')
 
